0535e76a4620f1f43692d90a17db5d81399347a3
Rafael <rafaelbitencourrt@gmail.com>
2022-01-20 13:01:48 -0300

    ok

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const Reader = require("./Reader");
+const Processor = require("./processor");
 
 var leitor = new Reader();
 
 async function main() {
   var dados = leitor.Read("./Pasta1.csv");
+
+  Processor.Process(dados);
 }
 
 main();
---
+++ processor.js
@@ -0,0 +1,8 @@
+class Processor {
+  static Process(data) {
+    var a = data.split("\n");
+    console.log(a);
+  }
+}
+
+module.exports = Processor;
