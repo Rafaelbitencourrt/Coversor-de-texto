8aa805deb2b289743734ddf93a72be8c13893aba
Rafael <rafaelbitencourrt@gmail.com>
2022-01-22 22:26:56 -0300

    ok

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const Reader = require("./Reader");
+var Writer = require("./Writter");
 const Processor = require("./Processor");
 const Table = require("./Table");
 const HtmlParser = require("./HtmlParser");
-
 var leitor = new Reader();
+var escritor = new Writer();
 
 async function main() {
   var dados = await leitor.Read("./Pasta1.csv");
@@ -13,6 +14,8 @@ async function main() {
   var usuarios = new Table(dadosProcessados);
 
   var html = await HtmlParser.Parse(usuarios);
+
+  escritor.Writer(date.now() + ".html", html);
 
   console.log(html);
 }
